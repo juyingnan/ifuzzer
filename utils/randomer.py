25680a15c1a6af62fa07959559a26a22d48d554b
Yingnan Ju <bunny_gg@hotmail.com>
2015-04-03 02:27:39 +0800

    bug fixes

--- utils/randomer.py
+++ utils/randomer.py
@@ -1,7 +1,8 @@
 __author__ = 'bunny_gg'
+import sys
+sys.path.append("..")
 import random
 import string
-
 
 def random_string(min_length=0, max_length=32, have_upper=True, have_lower=True, have_digit=True, have_other=True):
     length = random.randint(min_length, max_length)
@@ -20,3 +21,11 @@ def random_string(min_length=0, max_length=32, have_upper=True, have_lower=True,
 def random_shuffle(shuffle_list):
     random.shuffle(shuffle_list)
 
+
+def generate_random_file(file_address, min_length=0, max_length=32):
+    f = file(file_address, mode='w', buffering=-1)
+    f.write(random_string(min_length, max_length))
+    f.close()
+
+
+# generate_random_file("test.img", 0, 6553600)
